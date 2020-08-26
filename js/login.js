74762b54166e3b4ce9555b9d9f075a946d0bd431
MiaJLee <angella555@naver.com>
2020-08-26 14:44:57 +0900

    ADD id/pw validation

--- js/login.js
+++ js/login.js
@@ -4,14 +4,23 @@ const loginInput = document.getElementsByClassName('container')[0];
 const loginBtn = document.getElementById('btn_login');
 const linkToMain = document.getElementsByTagName('a')[0];
 
+function idCheck() {
+    var hasAt = idInput.value.indexOf('@');
+    return hasAt !== -1 ? true : false;
+}
+
+function pwCheck() {
+    return pwInput.value.length >= 5 ? true : false;
+}
+
 loginInput.addEventListener('keyup', function(event) {
-    const completedInput = !!(idInput.value && pwInput.value);
+    const completedInput = (idCheck() && pwCheck());
     loginBtn.disabled = completedInput ? false : true;
     linkToMain.href = completedInput ? "https://miajlee.github.io/westagram/main" : "#none"; 
 })
 
 document.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
-        document.getElementById("btn_login").click();
+        loginBtn.click();
     }
 })
